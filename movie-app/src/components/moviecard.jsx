e1b6426c71584bb8d6fb79941714c1ddf02d794d
Ganesh <00mr.lg00@gmail.com>
2024-04-15 15:50:59 +0530

    getting live trending movies and rendering them by map

---
+++ movie-app/src/components/moviecard.jsx
@@ -0,0 +1,6 @@
+
+export default function Moviecard(){
+    return(
+        <>..</>
+    )
+}
--- movie-app/src/components/trendingMovies.jsx
+++ movie-app/src/components/trendingMovies.jsx
@@ -1,63 +1,38 @@
+import { useEffect, useState } from "react"
+import Moviecard from "./moviecard"
+import axios from "axios"
+
+
 export default function Trendingmovies(){
-    return(
+  const [movies, setMovies] = useState([])
+
+  useEffect(()=>{
+       axios.get('https://api.themoviedb.org/3/trending/movie/day?api_key=e8f62da5e2126af5d78d9b0d4bc4d1ce')
+       .then((response)=>{
+          let moviesarray = response.data.results
+           setMovies(moviesarray);
+       })
+  },[])
+
+
+ if(movies.length==0){
+    return<>...loading</>
+ }
+
+return(
         <>
         <div className="text-center font-bold text-2xl	 mt-9 ">Trending Movies</div>
         <div className="flex flex-wrap	p-4 gap-6 justify-around ">
 
-        <div className=" h-[20rem]	w-[200px] rounded-xl overflow-hidden flex justify-center	items-end bg-cover	 bg-[url('https://weliveentertainment.com/wp-content/uploads/2024/02/madame-web-banner-4.jpg')]">
-            <div className=" text-slate-300	 text-lg bg-gray-800/50	w-full text-center p-2"	>Madame Web</div>
-        </div>
-        <div className=" h-[20rem]	w-[200px] rounded-xl overflow-hidden flex justify-center	items-end bg-cover	 bg-[url('https://weliveentertainment.com/wp-content/uploads/2024/02/madame-web-banner-4.jpg')]">
-            <div className=" text-slate-300	 text-lg bg-gray-800/50	w-full text-center p-2"	>Madame Web</div>
-        </div>
-        <div className=" h-[20rem]	w-[200px] rounded-xl overflow-hidden flex justify-center	items-end bg-cover	 bg-[url('https://weliveentertainment.com/wp-content/uploads/2024/02/madame-web-banner-4.jpg')]">
-            <div className=" text-slate-300	 text-lg bg-gray-800/50	w-full text-center p-2"	>Madame Web</div>
-        </div>
-        <div className=" h-[20rem]	w-[200px] rounded-xl overflow-hidden flex justify-center	items-end bg-cover	 bg-[url('https://weliveentertainment.com/wp-content/uploads/2024/02/madame-web-banner-4.jpg')]">
-            <div className=" text-slate-300	 text-lg bg-gray-800/50	w-full text-center p-2"	>Madame Web</div>
-        </div>
-        <div className=" h-[20rem]	w-[200px] rounded-xl overflow-hidden flex justify-center	items-end bg-cover	 bg-[url('https://weliveentertainment.com/wp-content/uploads/2024/02/madame-web-banner-4.jpg')]">
-            <div className=" text-slate-300	 text-lg bg-gray-800/50	w-full text-center p-2"	>Madame Web</div>
-        </div>
-        <div className=" h-[20rem]	w-[200px] rounded-xl overflow-hidden flex justify-center	items-end bg-cover	 bg-[url('https://weliveentertainment.com/wp-content/uploads/2024/02/madame-web-banner-4.jpg')]">
-            <div className=" text-slate-300	 text-lg bg-gray-800/50	w-full text-center p-2"	>Madame Web</div>
-        </div>
-        <div className=" h-[20rem]	w-[200px] rounded-xl overflow-hidden flex justify-center	items-end bg-cover	 bg-[url('https://weliveentertainment.com/wp-content/uploads/2024/02/madame-web-banner-4.jpg')]">
-            <div className=" text-slate-300	 text-lg bg-gray-800/50	w-full text-center p-2"	>Madame Web</div>
-        </div>
-        <div className=" h-[20rem]	w-[200px] rounded-xl overflow-hidden flex justify-center	items-end bg-cover	 bg-[url('https://weliveentertainment.com/wp-content/uploads/2024/02/madame-web-banner-4.jpg')]">
-            <div className=" text-slate-300	 text-lg bg-gray-800/50	w-full text-center p-2"	>Madame Web</div>
-        </div>
-        <div className=" h-[20rem]	w-[200px] rounded-xl overflow-hidden flex justify-center	items-end bg-cover	 bg-[url('https://weliveentertainment.com/wp-content/uploads/2024/02/madame-web-banner-4.jpg')]">
-            <div className=" text-slate-300	 text-lg bg-gray-800/50	w-full text-center p-2"	>Madame Web</div>
-        </div>
-        <div className=" h-[20rem]	w-[200px] rounded-xl overflow-hidden flex justify-center	items-end bg-cover	 bg-[url('https://weliveentertainment.com/wp-content/uploads/2024/02/madame-web-banner-4.jpg')]">
-            <div className=" text-slate-300	 text-lg bg-gray-800/50	w-full text-center p-2"	>Madame Web</div>
-        </div>
-        <div className=" h-[20rem]	w-[200px] rounded-xl overflow-hidden flex justify-center	items-end bg-cover	 bg-[url('https://weliveentertainment.com/wp-content/uploads/2024/02/madame-web-banner-4.jpg')]">
-            <div className=" text-slate-300	 text-lg bg-gray-800/50	w-full text-center p-2"	>Madame Web</div>
-        </div>
-        <div className=" h-[20rem]	w-[200px] rounded-xl overflow-hidden flex justify-center	items-end bg-cover	 bg-[url('https://weliveentertainment.com/wp-content/uploads/2024/02/madame-web-banner-4.jpg')]">
-            <div className=" text-slate-300	 text-lg bg-gray-800/50	w-full text-center p-2"	>Madame Web</div>
-        </div>
-        <div className=" h-[20rem]	w-[200px] rounded-xl overflow-hidden flex justify-center	items-end bg-cover	 bg-[url('https://weliveentertainment.com/wp-content/uploads/2024/02/madame-web-banner-4.jpg')]">
-            <div className=" text-slate-300	 text-lg bg-gray-800/50	w-full text-center p-2"	>Madame Web</div>
-        </div>
-        <div className=" h-[20rem]	w-[200px] rounded-xl overflow-hidden flex justify-center	items-end bg-cover	 bg-[url('https://weliveentertainment.com/wp-content/uploads/2024/02/madame-web-banner-4.jpg')]">
-            <div className=" text-slate-300	 text-lg bg-gray-800/50	w-full text-center p-2"	>Madame Web</div>
-        </div>
-        <div className=" h-[20rem]	w-[200px] rounded-xl overflow-hidden flex justify-center	items-end bg-cover	 bg-[url('https://weliveentertainment.com/wp-content/uploads/2024/02/madame-web-banner-4.jpg')]">
-            <div className=" text-slate-300	 text-lg bg-gray-800/50	w-full text-center p-2"	>Madame Web</div>
-        </div>
-        <div className=" h-[20rem]	w-[200px] rounded-xl overflow-hidden flex justify-center	items-end bg-cover	 bg-[url('https://weliveentertainment.com/wp-content/uploads/2024/02/madame-web-banner-4.jpg')]">
-            <div className=" text-slate-300	 text-lg bg-gray-800/50	w-full text-center p-2"	>Madame Web</div>
-        </div>
-        <div className=" h-[20rem]	w-[200px] rounded-xl overflow-hidden flex justify-center	items-end bg-cover	 bg-[url('https://weliveentertainment.com/wp-content/uploads/2024/02/madame-web-banner-4.jpg')]">
-            <div className=" text-slate-300	 text-lg bg-gray-800/50	w-full text-center p-2"	>Madame Web</div>
-        </div>
-        <div className=" h-[20rem]	w-[200px] rounded-xl overflow-hidden flex justify-center	items-end bg-cover	 bg-[url('https://weliveentertainment.com/wp-content/uploads/2024/02/madame-web-banner-4.jpg')]">
-            <div className=" text-slate-300	 text-lg bg-gray-800/50	w-full text-center p-2"	>Madame Web</div>
-        </div>
+         { movies.map((movieObj)=>{
+           return <div className=" h-[20rem]	w-[200px] rounded-xl overflow-hidden flex justify-center	items-end bg-cover"	 
+                  style={{backgroundImage:`url(https://image.tmdb.org/t/p/original${movieObj.poster_path})`}} >
+
+                 <div className=" text-slate-300	 text-lg bg-gray-800/50	w-full text-center p-2"	>{movieObj.title}</div>
+              </div>
+         })}
+
+        
 
         </div>
 
